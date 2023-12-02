fbb0b90e879bc8b03d3eb189bfe0c7adfbf56852
Adrien <adrien.gaillard-lafargue@epitech.eu>
2023-12-02 17:27:23 +0100

    Fixing dependencies for player and andding game scene

--- Scenes/Characters/Player/Player.gd
+++ Scenes/Characters/Player/Player.gd
@@ -17,12 +17,14 @@ func _process(delta):
 func _input(event : InputEvent):
 	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	direction = direction.normalized()
 	
-	if (direction == Vector2(1, 0)):
-		animated_sprite.play("MoveRight")
-	if (direction == Vector2(-1, 0)):
-		animated_sprite.play("MoveLeft")
-	if (direction == Vector2(0, 1)):
-		animated_sprite.play("MoveDown")
-	if (direction == Vector2(0, -1)):
-		animated_sprite.play("MoveUp")
+	if (direction == Vector2.ZERO):
+		animated_sprite.stop()
+		animated_sprite.set_frame(0)
+
+	match(direction):
+		Vector2.RIGHT: animated_sprite.play("MoveRight")
+		Vector2.LEFT: animated_sprite.play("MoveLeft")
+		Vector2.DOWN: animated_sprite.play("MoveDown")
+		Vector2.UP: animated_sprite.play("MoveUp")
